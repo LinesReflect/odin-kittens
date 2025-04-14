14e67fe6074c2bb1552e4c7fa14dc78322ee133d
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-04-14 13:50:46 -0500

    Create and fill out method for each kitten route

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,46 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def kitten_params
+    params.expect(kitten: [ :name, :age, :cuteness, :softness ])
+  end
 end
